b5e175f69c183cc9d0221cb85b740416efc4529c
tocchimittsu <arimagedon21@gmail.com>
2021-05-20 16:08:30 +0900

    修正

---
+++ README.md
@@ -0,0 +1,5 @@
+# tkinter-listbox
+Tkinterのlistboxに関するコード
+
+# 参考記事
+https://kuroro.blog/python/XMWVRR2MEZAe4bpPDDXE/
---
+++ main.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+
+class Application(tk.Frame):
+    # textに関する情報を格納する変数
+    text = None
+
+    def __init__(self, master=None):
+        # Windowの初期設定を行う。
+        super().__init__(master)
+
+        # Windowの画面サイズを設定する。
+        self.master.geometry("300x200")
+
+        # Windowを親要素として、frame Widget(Frame)を作成する。
+        # frameについて : https://kuroro.blog/python/P20XOidA5nh583fYRvxf/
+        frame = tk.Frame(self.master)
+
+        # Windowを親要素とした場合に、frame Widgetをどのように配置するのか?
+        # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
+        frame.pack()
+
+        # Frameを親要素として、text Widgetを作成する。
+        self.text = tk.Text(frame)
+
+        # Frameを親要素とした場合に、text Widgetをどのように配置するのか?
+        # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
+        self.text.pack()
+
+# Tkinter初学者参考 : https://docs.python.org/ja/3/library/tkinter.html#a-simple-hello-world-program
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = Application(master=root)
+    # Windowをループさせて、継続的にWindow表示させる。
+    app.mainloop()
